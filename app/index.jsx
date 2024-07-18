e869dee11fd0829c330a09cd526593c8bcfc1345
Altono1 <altonobinna11@gmail.com>
2024-07-18 18:22:40 +0100

    v9

--- app/index.jsx
+++ app/index.jsx
@@ -1,27 +1,23 @@
-import { Redirect } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Redirect } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import * as Font from "expo-font";
 
 export default function Index() {
-
-  const [fontLoaded, setFontLoaded] = useState(false)
+  const [fontLoaded, setFontLoaded] = useState(false);
 
   useEffect(() => {
     Font.loadAsync({
-      "outfit": require("../assets/fonts/Outfit-Regular.ttf"),
-      'outfit-medium':require('../assets/fonts/Outfit-Medium.ttf'),
-      'outfit-bold':require('../assets/fonts/Outfit-Bold.ttf')
-    })
-    .then(() => {
-     setFontLoaded(true)
-    }) 
-  }, [])
+      outfit: require("../assets/fonts/Outfit-Regular.ttf"),
+      "outfit-medium": require("../assets/fonts/Outfit-Medium.ttf"),
+      "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
+    }).then(() => {
+      setFontLoaded(true);
+    });
+  }, []);
 
-  if (!fontLoaded) return null
-  return (
-     <Redirect href={'/home'} />
-  );
+  if (!fontLoaded) return null;
+
+  return <Redirect href={"/home"} />;
 }
-
