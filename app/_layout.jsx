ca075c2c4cb4be299bd8cc8bf9d843ba64c67c5d
Altono1 <altonobinna11@gmail.com>
2024-08-06 20:31:13 +0100

    splash

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,49 +1,60 @@
+import React from "react";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import LoginScreen from "../components/LoginScreen";
+import * as SecureStore from "expo-secure-store";
 
-import React from 'react'
-import { Stack } from 'expo-router'
-import { useFonts } from "expo-font"
-import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo'
-import { Text } from 'react-native'
-import LoginScreen from '../components/LoginScreen'
-import * as SecureStore from 'expo-secure-store'
+//this stops the splash screen from hiding when fonts have not loaded
+SplashScreen.preventAutoHideAsync();
 
 const tokenCache = {
   async getToken(key) {
     try {
       return SecureStore.getItemAsync(key);
     } catch (err) {
-      return null
+      return null;
     }
   },
   async saveToken(key, value) {
     try {
-      return SecureStore.setItemAsync(key, value)
+      return SecureStore.setItemAsync(key, value);
     } catch (err) {
-      return
+      return;
     }
-  }
-}
+  },
+};
 
-export default function RootLayout () {
+export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    outfit: require("./../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
+  });
 
-  useFonts({
-    'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
-    'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
-    'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf')
-  })
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
+
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-      <SignedIn>  
-    <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
-   </Stack>
-   </SignedIn> 
-   <SignedOut>
-    <LoginScreen />
-   </SignedOut>
-   </ClerkProvider>
-  )
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <SignedIn>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <LoginScreen />
+      </SignedOut>
+    </ClerkProvider>
+  );
 }
-
-
-
