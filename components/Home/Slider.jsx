561e8356f6e49b25cdc97bb72a1abab85cfe90ce
Altono1 <altonobinna11@gmail.com>
2025-01-15 15:06:28 +0100

    homecard

--- components/Home/Slider.jsx
+++ components/Home/Slider.jsx
@@ -1,56 +1,58 @@
-import { View, Text, FlatList, Image } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { collection, getDocs, query } from 'firebase/firestore'
-import { db } from '../../configs/FirebaseConfig'
+import { View, Text, FlatList, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../../configs/FirebaseConfig";
 
 export default function Slider() {
+  const [sliderList, setSliderList] = useState([]);
 
-    const [sliderList, setSliderList] = useState([])
+  const GetSliderList = async () => {
+    setSliderList([]);
+    const q = query(collection(db, "Slider"));
+    const querySnapshot = await getDocs(q);
 
-    const GetSliderList = async () => {
-        setSliderList([])
-        const q = query(collection(db, 'Slider'))
-        const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc) => {
+      setSliderList((prev) => [...prev, doc.data()]);
+    });
+  };
 
-        querySnapshot.forEach((doc) => {
-            setSliderList((prev) => [...prev, doc.data()])
-        })
-    }
-
-    useEffect(() => {
-        GetSliderList()
-    }, [])
-
+  useEffect(() => {
+    GetSliderList();
+  }, []);
 
   return (
     <View>
-      <Text  style={{
-        fontFamily: 'outfit-bold',
-        fontSize: 20,
-        paddingLeft: 20,
-        paddingTop: 20,
-        marginBottom: 15
-      }} >
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 20,
+          paddingLeft: 20,
+          paddingTop: 20,
+          marginBottom: 15,
+        }}
+      >
         #Special for you
       </Text>
-      <FlatList data={sliderList}
-       horizontal
-       showsHorizontalScrollIndicator={false}
-       style={{
-        paddingLeft: 20
-       }}
-      renderItem={({item, index}) => (
-        <Image source={{ uri: item.imageUrl}}
+      <FlatList
+        data={sliderList}
+        horizontal
+        showsHorizontalScrollIndicator={false}
         style={{
-            width: 300,
-            height: 150,
-            borderRadius: 15,
-            marginRight: 15
+          paddingLeft: 20,
         }}
-       
-        />
-      )}
+        renderItem={({ item, index }) => (
+          <Image
+            source={{ uri: item.imageUrl }}
+            key={index}
+            style={{
+              width: 300,
+              height: 150,
+              borderRadius: 15,
+              marginRight: 15,
+            }}
+          />
+        )}
       />
     </View>
-  )
+  );
 }
